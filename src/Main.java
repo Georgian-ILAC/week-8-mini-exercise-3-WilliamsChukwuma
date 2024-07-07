9d2c1bf868f5382e873b8ff3101dfa4be6fe8e6e
Williams Chukwuma <WilliamsChukwuma@users.noreply.github.com>
2024-07-06 20:45:44 -0400

    completed mini excercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee EmployeeOne = new Employee();
+        System.out.println(EmployeeOne.toString());
     }
 }
